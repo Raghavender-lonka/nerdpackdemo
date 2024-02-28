a4a207c964f38c888bd45bfa31a508b64cb7c8f1
Raghavender-lonka <raghavenderreddylonka.1@gmail.com>
2024-02-28 20:37:37 +0530

    changed text

--- visualizations/demo/index.js
+++ visualizations/demo/index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default class DemoVisualization extends React.Component {
   render() {
-    return <h1>Hello World!1</h1>
+    return <h1>Hello World!2</h1>
   }
 }
